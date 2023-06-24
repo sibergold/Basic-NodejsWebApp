2a291137788ab880780ca1b9e0c2b7f76a8e513e
sibergold <kaynakca21@hotmail.com>
2023-06-24 18:17:17 +0300

    Proje dosyası ekle.

---
+++ app.js
@@ -0,0 +1,21 @@
+let isim = prompt("Lutfen isminizi giriniz:");
+let myName = document.querySelector("#myName");
+myName.innerHTML = ` ${isim}`;
+
+function zaman() {
+
+    const tarih = new Date;
+
+    let saat = tarih.getHours();
+    let dakika = tarih.getMinutes();
+    let saniye = tarih.getSeconds();
+
+
+    let gun = ["Pazartesi", "Salý", "Çarþamba", "Perþembe", "Cuma", "Cumartesi", "Pazar"];
+    let gunName = gun[tarih.getDay()]
+
+    let clock = document.querySelector("#myClock");
+    clock.innerHTML = `${saat}:${dakika}:${saniye} ${gunName}`;
+}
+
+setInterval(zaman, 1000);
